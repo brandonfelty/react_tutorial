e20674c8780e26d8efc83c2ccc4be83f4a4de969
Brandon Felty <brcfelty@gmail.com>
2022-11-07 12:40:47 -0700

    practice jsx

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-// const NavBar = () => {
-//   return (
-//   <>
-//     NavBar
-//   </>);
-// }
-
-// const MainBody = () => {
-//   return (
-//     <p>I'm the main body</p>
-//   )
-// }
-
-// ReactDOM.render(
-//   <div>
-//     <NavBar />
-//     <MainBody />
-//   </div>,
-//   document.getElementById('root')
-// );
-
-// const h1 = document.createElement('h1');
-// h1.textContent = 'Imperative way to program';
-// h1.className = 'header';
-// document.getElementById('root').appendChild(h1);
-
-const page = (
-  <div>
-    <h1 className='header'>This is JSX</h1>
-    <p>This is a paragraph</p>
-  </div>
+const navbar = (
+    <nav>
+      <h1>JiggleyPuff</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
 )
 
-console.log(page)
-
 ReactDOM.render(
-  page,
+  navbar,
   document.getElementById('root')
 )
