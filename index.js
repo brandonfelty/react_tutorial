b35ecc6491ce4808fe0b0ceb7499988368ea3ba8
Brandon Felty <brcfelty@gmail.com>
2022-11-07 12:33:45 -0700

    familiarizing with jsx

--- index.js
+++ index.js
@@ -19,7 +19,21 @@
 //   document.getElementById('root')
 // );
 
-const h1 = document.createElement('h1');
-h1.textContent = 'Imperative way to program';
-h1.className = 'header';
-document.getElementById('root').appendChild(h1);
+// const h1 = document.createElement('h1');
+// h1.textContent = 'Imperative way to program';
+// h1.className = 'header';
+// document.getElementById('root').appendChild(h1);
+
+const page = (
+  <div>
+    <h1 className='header'>This is JSX</h1>
+    <p>This is a paragraph</p>
+  </div>
+)
+
+console.log(page)
+
+ReactDOM.render(
+  page,
+  document.getElementById('root')
+)
