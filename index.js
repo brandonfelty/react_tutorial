fd41280e55df4a22b41c230419ddf964445c28e6
Brandon Felty <brcfelty@gmail.com>
2022-11-07 12:22:46 -0700

    declarative program practice

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const NavBar = () => {
-  return (
-  <>
-    NavBar
-  </>);
-}
+// const NavBar = () => {
+//   return (
+//   <>
+//     NavBar
+//   </>);
+// }
 
-const MainBody = () => {
-  return (
-    <p>I'm the main body</p>
-  )
-}
+// const MainBody = () => {
+//   return (
+//     <p>I'm the main body</p>
+//   )
+// }
 
-ReactDOM.render(
-  <div>
-    <NavBar />
-    <MainBody />
-  </div>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <div>
+//     <NavBar />
+//     <MainBody />
+//   </div>,
+//   document.getElementById('root')
+// );
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Imperative way to program';
+h1.className = 'header';
+document.getElementById('root').appendChild(h1);
