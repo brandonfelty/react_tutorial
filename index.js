02ff0494d02743df9fe3dc25c2f7d2b93acbf4ce
Brandon Felty <brcfelty@gmail.com>
2022-11-07 12:08:41 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const MainBody = () => {
+  return (
+    <p>I'm the main body</p>
+  )
+}
+
+ReactDOM.render(
+  <ul>
+    <li>item 1</li>
+    <li>item 2</li>
+  </ul>,
+  document.getElementById('root')
+);
