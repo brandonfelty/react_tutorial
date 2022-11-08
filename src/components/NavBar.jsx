38290a3504942c37bdbc6a28e9ae4980d401808a
Brandon Felty <brcfelty@gmail.com>
2022-11-08 13:51:12 -0700

    navbar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import reactLogo from '../react.png';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <h1>NavBar</h1>
+    <div className='nav-container'>
+      <img alt='react-logo' src={reactLogo} className='img-logo'/>
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </div>
   )
 }
 
