9c33f14f9857a38fcabd53921a702aa00b1719af
Brandon Felty <brcfelty@gmail.com>
2022-11-08 08:02:41 -0700

    creates page component

---
+++ src/components/Page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <>
+      <h1>Reasons I like React</h1>
+      <ul>
+        <li>Potential to work remote</li>
+        <li>Well paid profession</li>
+        <li>Career seems more relaxed</li>
+        <li>Higher likelyhood for 4-day week</li>
+      </ul>
+    </>
+  )
+}
+
+export default Page;
