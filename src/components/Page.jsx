bb3fe5d639f967760282999ff449391620245850
Brandon Felty <brcfelty@gmail.com>
2022-11-08 10:21:58 -0700

    style app

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Page.css';
 
 function Page() {
   return (
-    <>
-      <h1>Reason's I like React</h1>
-      <ol>
+    <div className='page-container'>
+      <h1 className='page-title'>Reason's I like React</h1>
+      <ul className='page-list'>
         <li>Potential to work remote</li>
         <li>Well paid profession</li>
         <li>Career seems more relaxed</li>
         <li>Higher likelyhood for 4-day week</li>
-      </ol>
-    </>
+      </ul>
+    </div>
   )
 }
 
