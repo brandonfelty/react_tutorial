c15fdf3a4048002734330419124dafe48392f039
Brandon Felty <brcfelty@gmail.com>
2022-11-08 09:44:30 -0700

    adds styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import logo from '../react.png';
 function Nav() {
   return (
     <div className='nav-container'>
-      <img src={logo} alt='react' width='40px'/>
+      <img src={logo} alt='react' className='nav-img'/>
       <ul className='nav-items'>
         <li>Pricing</li>
         <li>About</li>
