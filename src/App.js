67f79974ea27038d192086551071456a1c729df0
Brandon Felty <brcfelty@gmail.com>
2022-11-08 08:18:02 -0700

    add nav comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Page from './components/Page';
 import Footer from './components/Footer';
-import logo from './react.png';
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} alt='react' width='40px'/>
+      
       <Page />
       <Footer />
     </div>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../react.png';
+
+function Nav() {
+  return (
+    <div>
+      <img src={logo} alt='react' width='40px'/>
+    </div>
+  )
+}
+
+export default Nav;
