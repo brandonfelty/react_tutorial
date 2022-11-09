1c321947bf55748a8f938ed7ad2a2b3c73f58260
Brandon Felty <brcfelty@gmail.com>
2022-11-09 14:14:57 -0700

    linkedin link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,16 @@ import './Button.css';
 
 const Button = ({ type }) => {
   return (
-    <button type='button' className={type}>
-      {type === 'email' && <i className="fa-solid fa-envelope"></i>} 
-      {type === 'linkedin' && <i class="fa-brands fa-linkedin"></i>} 
-      {type}
-    </button>
+    <form>
+      <button 
+        formAction={type === 'linkedin' ? 'https://www.linkedin.com/in/brcfelty/' : null}
+        className={type}
+      >
+        {type === 'email' && <i className="fa-solid fa-envelope"></i>} 
+        {type === 'linkedin' && <i class="fa-brands fa-linkedin"></i>} 
+        {type}
+      </button>
+  </form>
   )
 }
 
