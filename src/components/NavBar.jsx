040eb846c3f4d1d1c04ee4d39eb350233cf7c3f7
Brandon Felty <brcfelty@gmail.com>
2022-11-14 11:28:59 -0800

    change navbar for airbnb

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import reactLogo from '../react.png';
+import airbnbLogo from '../airbnb.png';
 import './NavBar.css';
 
-const NavBar = () => {
+const NavBar = ({ title, subtitle }) => {
+  let logo = '';
+  if (title === 'React') logo = reactLogo;
+  if (title === 'AirBnB') logo = airbnbLogo;
+
   return (
     <nav className='nav-container'>
-      <img alt='react-logo' src={reactLogo} className='img-logo'/>
-      <h3>ReactFacts</h3>
-      <h4>React Course - Project 1</h4>
+      <img 
+        alt='react-logo' 
+        src={logo} 
+        className='img-logo'
+      />
+      <h3>{title}</h3>
+      <h4>{subtitle}</h4>
       <a href='/info'> Business Card</a>
     </nav>
   )
