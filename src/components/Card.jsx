3b0ec3302d49b1ae3d238f57e51303da1cff2c9b
Brandon Felty <brcfelty@gmail.com>
2022-11-16 15:01:29 -0800

    add card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import background from '../palm Small.png';
+import './Card.css';
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <img src={background} alt='card'/>
+      <p>
+        5
+        <i className="fa-solid fa-star"></i>
+         (3) - USA
+      </p>
+      <p>Our lady palm with Katie Zolang</p>
+      <div className='rate'>
+        <p className='cost'>
+          $136
+        </p>
+        <p> / person</p>
+      </div>
+    </div>
   )
 }
 
