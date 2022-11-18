f48b184f1d3c848a9724915016d0609a6e6b6641
Brandon Felty <brcfelty@gmail.com>
2022-11-17 16:07:08 -0800

    add dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,14 @@
+import palm from './images/palm small.png';
+
+export const expData = [
+  {
+    img: palm,
+    rating: 
+    {
+      amount: 3,
+      location: 'USA'  
+    },
+    title: 'Our lady palm with Andy Zolang',
+    cost: 136
+  }
+];
