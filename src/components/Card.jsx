426fcc32b3fa4e80a57a996e672c6e83e21e9af2
Brandon Felty <brcfelty@gmail.com>
2022-11-17 16:33:19 -0800

    add cards data and style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,23 +3,25 @@ import './Card.css';
 import { expData } from '../dummyData';
 
 const Card = () => {
-  const card = expData[0];
-
   return (
-    <div className='card'>
-      <img src={card.img} alt='card' className='cover'/>
-      <p className='rating'>
-        {card.rating.stars}
-        <i className="fa-solid fa-star rating-star"></i>
-         ({card.rating.amount}) - {card.rating.location}
-      </p>
-      <p>{card.title}</p>
-      <div className='rate'>
-        <p className='cost'>
-          From ${card.cost}&#160;
-        </p>
-        <p> / person</p>
-      </div>
+    <div className='card-container'>
+      { expData.map((card, index) => (
+        <div className='card' key={index}>
+          <img src={card.img} alt='card' className='cover'/>
+          <p className='rating'>
+            {card.rating.stars}
+            <i className="fa-solid fa-star rating-star"></i>
+            ({card.rating.amount}) - {card.rating.location}
+          </p>
+          <p>{card.title}</p>
+          <div className='rate'>
+            <p className='cost'>
+              From ${card.cost}&#160;
+            </p>
+            <p> / person</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
