239d44e42be8ee73174705c99044f88088f9813f
Brandon Felty <brcfelty@gmail.com>
2022-11-17 16:23:39 -0800

    add more data

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,4 +1,7 @@
 import palm from './images/palm Small.png';
+import forest from './images/forest Small.png';
+import elk from './images/elk Small.png';
+
 
 export const expData = [
   {
@@ -11,5 +14,26 @@ export const expData = [
     },
     title: 'Our lady palm with Andy Zolang',
     cost: 136
-  }
+  },
+  {
+    img: forest,
+    rating: 
+    {
+      stars: 5,
+      amount: 30,
+      location: 'CAN'  
+    },
+    title: 'Run Forest Run!',
+    cost: 170
+  },{
+    img: elk,
+    rating: 
+    {
+      stars: 4.8,
+      amount: 5,
+      location: 'USA'  
+    },
+    title: 'Do you elk what I elk?',
+    cost: 169
+  },
 ];
