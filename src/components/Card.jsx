ea15a3c7efae713fb15fb2fc5afc4be2d039398a
Brandon Felty <brcfelty@gmail.com>
2022-11-17 16:17:44 -0800

    add dummy data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import background from '../images/palm Small.png';
 import './Card.css';
+import { expData } from '../dummyData';
 
 const Card = () => {
+  const card = expData[0];
+
   return (
     <div className='card'>
-      <img src={background} alt='card' className='cover'/>
+      <img src={card.img} alt='card' className='cover'/>
       <p className='rating'>
-        5
+        {card.rating.stars}
         <i className="fa-solid fa-star rating-star"></i>
-         (3) - USA
+         ({card.rating.amount}) - {card.rating.location}
       </p>
       <p>Our lady palm with Katie Zolang</p>
       <div className='rate'>
         <p className='cost'>
-          From $136&#160;
+          From ${card.cost}&#160;
         </p>
         <p> / person</p>
       </div>
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,10 +1,11 @@
-import palm from './images/palm small.png';
+import palm from './images/palm Small.png';
 
 export const expData = [
   {
     img: palm,
     rating: 
     {
+      stars: 5,
       amount: 3,
       location: 'USA'  
     },
