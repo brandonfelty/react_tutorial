8026ce9f5b1f7a36864a9d3fbe9000fb8d174ec2
Brandon Felty <brcfelty@gmail.com>
2022-11-23 13:29:42 -0800

    map entry data

--- src/pages/TravelJournal.jsx
+++ src/pages/TravelJournal.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
 import Entry from '../components/Entry';
+import { travelJournalData } from '../dummyData';
 
 const TravelJournal = () => {
   return (
     <div>
       <NavBar title='My Travel Journal' pic='umbrella'/>
-      <Entry />
+      {travelJournalData.map((travelItem, index) => (
+        <Entry 
+          key={index}
+          travelItem={travelItem}
+        />
+      ))}
     </div>
   )
 }
