fea8002101082dd86472cbb69324354051ab7946
Brandon Felty <brcfelty@gmail.com>
2022-11-23 08:21:02 -0800

    adds badge text challenge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const Card = ({ card }) => {
+  let badgeText;
+  if (card.openSpots === 0 && !card.fire) {
+    badgeText = 'SOLD OUT'
+  } else if (card.fire) {
+    badgeText = 'THIS IS FIRE'
+  } else {
+    badgeText = '';
+  }
 
   return (
     <div className='card'>
       <div className='img-container'>
         {card.fire && <i className="fa-solid fa-fire"></i>}
+        <div className='badge-text'>{badgeText}</div>
         <img src={card.img} alt='card' className='cover'/>
       </div>
       <p className='rating'>
