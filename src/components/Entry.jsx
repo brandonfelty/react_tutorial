f595fcaac3c3f57a7ce05184853d175b2b622de7
Brandon Felty <brcfelty@gmail.com>
2022-11-23 13:47:42 -0800

    displays all data in the entry

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Entry = () => {
+const Entry = ({ travelItem }) => {
+  const imgNameArray = Object.keys(travelItem.imgURL);
+  const imgName = imgNameArray[0];
+
   return (
-    <div>Entry</div>
+    <div className='travel-entry'>
+      <img alt='travel' src={travelItem.imgURL[imgName]} />
+      <div className='travel-details'>
+      <div className='travel-location'>
+        <i class="fa-solid fa-location-dot"></i>
+        {travelItem.location}
+        <a href={travelItem.googleMapsLink}>Google Maps Link</a>
+      </div>
+      <h1>{travelItem.title}</h1>
+      <h5>{travelItem.startDate} - {travelItem.endDate}</h5>
+      <p>{travelItem.description}</p>
+      </div>
+    </div>
   )
 }
 
