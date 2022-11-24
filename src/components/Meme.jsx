4f323974d9f3a14a6843f40524f6187c46952086
Brandon Felty <brcfelty@gmail.com>
2022-11-24 10:58:20 -0800

    adds all details to meme

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Meme = () => {
   return (
-    <div>Meme</div>
+    <div className='meme'>
+      <form>
+        <div className='meme-inputs'>
+          <input placeholder='shut up'></input>
+          <input placeholder='and take my money'></input>
+        </div>
+        <button type='button'>
+          <p>Get a new meme image</p>
+          <i className="fa-solid fa-image"></i>
+        </button>
+      </form>
+    </div>
   )
 }
 
