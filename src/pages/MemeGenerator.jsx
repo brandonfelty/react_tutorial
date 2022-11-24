b249b6d07a6123850009e79e0e1eea5706eca630
Brandon Felty <brcfelty@gmail.com>
2022-11-24 09:00:08 -0800

    adds nav with props

--- src/pages/MemeGenerator.jsx
+++ src/pages/MemeGenerator.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 
 const MemeGenerator = () => {
   return (
-    <div>MemeGenerator</div>
+    <div className='meme--page'>
+      <NavBar 
+        title='Meme Generator'
+        subtitle='React Course - Project 3'
+        pic='meme-logo'
+      />
+    </div>
   )
 };
 
