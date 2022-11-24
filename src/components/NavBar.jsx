5e823eaf6053b7e58c320b64572d0e702dad21f2
Brandon Felty <brcfelty@gmail.com>
2022-11-24 09:18:49 -0800

    create react component from svg and add to nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import reactLogo from '../images/react.png';
 import airbnbLogo from '../images/airbnb.png';
+import MemeLogo from './MemeLogo';
 import './NavBar.css';
 
 const NavBar = ({ title, subtitle, pic }) => {
   let logo = '';
   if (pic === 'react') logo = reactLogo;
   if (pic === 'airbnb') logo = airbnbLogo;
-  let umbrella;
-  if (pic === 'umbrella') umbrella = <i className="fa-solid fa-umbrella"></i>;
-
+  let logoComponent;
+  if (pic === 'umbrella') logoComponent = <i className="fa-solid fa-umbrella"></i>;
+  if (pic === 'meme-logo') logoComponent = <MemeLogo />;
 
   return (
     <nav className={pic === 'umbrella' ? 'nav-travel': 'nav-container' }>
-      {!umbrella ? <img 
+      {!logoComponent ? <img 
         alt='react-logo' 
         src={logo} 
         className='img-logo'
       /> :
-      umbrella}
+      logoComponent}
       <h3>{title}</h3>
       <h4>{subtitle}</h4>
       <a href='/info'> Business Card</a>
