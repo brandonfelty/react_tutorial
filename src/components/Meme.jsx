8cf3adc562fd65856304ca6b9a775d199bbab63c
Brandon Felty <brcfelty@gmail.com>
2022-11-24 11:25:01 -0800

    styles inputs and button

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './Meme.css';
 
 const Meme = () => {
   return (
@@ -8,7 +9,7 @@ const Meme = () => {
           <input placeholder='shut up'></input>
           <input placeholder='and take my money'></input>
         </div>
-        <button type='button'>
+        <button type='submit'>
           <p>Get a new meme image</p>
           <i className="fa-solid fa-image"></i>
         </button>
