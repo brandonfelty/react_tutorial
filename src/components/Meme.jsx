56643f569d7381039e8212ac70803404b0f05ee2
Brandon Felty <brcfelty@gmail.com>
2022-11-28 08:56:37 -0800

    add handle click func

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './Meme.css';
 
 const Meme = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('I was clicked..')
+  }
   return (
     <div className='meme'>
       <form>
@@ -9,7 +13,10 @@ const Meme = () => {
           <input placeholder='shut up'></input>
           <input placeholder='and take my money'></input>
         </div>
-        <button type='submit'>
+        <button 
+          type='submit'
+          onClick={handleClick}
+        >
           <p>Get a new meme image</p>
           <i className="fa-solid fa-image"></i>
         </button>
