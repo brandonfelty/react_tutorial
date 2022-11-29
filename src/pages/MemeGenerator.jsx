28c0c3753dc063c7ec59ce44b6b8b1daf5548032
Brandon Felty <brcfelty@gmail.com>
2022-11-29 11:07:20 -0800

    finish challenge

--- src/pages/MemeGenerator.jsx
+++ src/pages/MemeGenerator.jsx
@@ -4,6 +4,8 @@ import Meme from '../components/Meme';
 import './MemeGenerator.css'
 
 const MemeGenerator = () => {
+  const [result, setResult] = React.useState('Yes');
+
   return (
     <div className='meme--page'>
       <NavBar 
@@ -15,7 +17,7 @@ const MemeGenerator = () => {
       <div className='state'>
         <h1>Is state important to know?</h1>
         <div className='state-answer'>
-          <h1>Yes</h1>
+          <h1>{result}</h1>
         </div>
       </div>
     </div>
