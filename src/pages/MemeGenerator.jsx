3056b99ddde83cb661468e194544817a454aac7b
Brandon Felty <brcfelty@gmail.com>
2022-11-30 09:03:58 -0800

    remove challenge

--- src/pages/MemeGenerator.jsx
+++ src/pages/MemeGenerator.jsx
@@ -4,13 +4,7 @@ import Meme from '../components/Meme';
 import './MemeGenerator.css'
 
 const MemeGenerator = () => {
-  const [result, setResult] = React.useState('Yes');
-  const handleClick = () => {
-    let change;
-    if (result === 'Yes') change = 'No';
-    if (result === 'No') change = 'Yes';
-    setResult(change)
-  }
+
   return (
     <div className='meme--page'>
       <NavBar 
@@ -19,14 +13,7 @@ const MemeGenerator = () => {
         pic='meme-logo'
       />
       <Meme />
-      <div className='state'>
-        <h1
-          onClick={handleClick}
-        >Is state important to know?</h1>
-        <div className='state-answer'>
-          <h1>{result}</h1>
-        </div>
-      </div>
+      
     </div>
   )
 };
