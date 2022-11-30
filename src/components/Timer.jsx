fa2d1f9772080c300db133bc4bc1e38e9eb1a23c
Brandon Felty <brcfelty@gmail.com>
2022-11-30 09:39:27 -0800

    styled counter component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,13 +4,13 @@ import './Timer.css';
 const Timer = () => {
   return (
     <div className='timer--container'>
-      <div className='timer--counter-container'>
+      <div className='timer--counter-container minus'>
         <i className="fa-solid fa-minus timer--counter"></i>
       </div>
       <div className='timer--count'>
         <h1>0</h1>
       </div>
-      <div className='timer--counter-container'>
+      <div className='timer--counter-container plus'>
         <i className="fa-solid fa-plus timer--counter"></i>
       </div>
     </div>
