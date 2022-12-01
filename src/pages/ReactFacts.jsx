9643516200c10d04be779b523b4ab03b32caa0cb
Brandon Felty <brcfelty@gmail.com>
2022-12-01 11:46:09 -0800

    sets up welcome component

--- src/pages/ReactFacts.jsx
+++ src/pages/ReactFacts.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from "../components/NavBar";
 import Main from "../components/Main";
 import Timer from '../components/Timer';
+import Welcome from '../components/Welcome';
 
 const ReactFacts = () => {
   return (
     <>
       <NavBar pic='react' title='React' subtitle='Lesson 1 - React Facts'/>
       <Main />
+      <Welcome />
       <Timer />
     </>
   )
