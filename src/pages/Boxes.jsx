0c37c161c3ed470287b806c2dbfc15589ea7ac9b
Brandon Felty <brcfelty@gmail.com>
2022-12-05 08:45:32 -0800

    remove render logic

--- src/pages/Boxes.jsx
+++ src/pages/Boxes.jsx
@@ -6,17 +6,17 @@ import Box from '../components/Box';
 const Boxes = () => {
   const [ boxes ] = React.useState(boxData);
 
-  const toggle = () => {
-    console.log('clicked!')
+  const toggle = (id) => {
+    console.log(id)
   }
 
   return (
     <div className='boxes'>
       {
         boxes.map((box) => (
-          box.on && 
           <Box
             key={box.id}
+            id={box.id}
             on={box.on}
             toggle={toggle}
           />
