3a81686caf9cc8ef25422157131b4e0d0711b221
Brandon Felty <brcfelty@gmail.com>
2022-12-06 07:53:30 -0800

    allows user to toggle punchline

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Joke = (props) => {
+  const [ isShown, setIsShown ] = React.useState(false);
+
+  const toggleShown = (e) => {
+    e.preventDefault();
+    setIsShown(prev => !prev);
+  };
+
+
   return (
     <div className='joke'>
       <h1>Setup: {props.setup}</h1>
-      <h3>PunchLine: {props.punchline}</h3>
+      <button onClick={toggleShown} >Show PunchLine</button>
+      { isShown && <h3>PunchLine: {props.punchline}</h3> }
     </div>
   )
 }
