60ad56d7bb1caed3906383e51969b712cafa0ba8
Brandon Felty <brcfelty@gmail.com>
2022-12-08 07:16:00 -0700

    set first name state for form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+  const [ firstName, setFirstName ] = useState('');
+
+  const handleChange = (e) => {
+    setFirstName(e.target.value)
+  }
+  
   return (
     <form>
       <input
         type="text"
         placeholder='First Name'
+        onChange={handleChange}
       />
     </form>
   )
