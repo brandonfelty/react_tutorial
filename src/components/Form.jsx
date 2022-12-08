d2513d086a1fb26e70b6e6b080de641cf882f3e7
Brandon Felty <brcfelty@gmail.com>
2022-12-08 07:29:10 -0700

    add last name to form and state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,16 +2,32 @@ import React, { useState } from 'react'
 
 function Form() {
   const [ firstName, setFirstName ] = useState('');
+  const [ lastName, setLastName ] = useState('');
 
   const handleChange = (e) => {
-    setFirstName(e.target.value)
+    const inputBox = e.target.placeholder;
+    switch (inputBox) {
+      case 'First Name':
+        setFirstName(e.target.value);
+        break;
+      case 'Last Name':
+        setLastName(e.target.value);
+        break;
+      default:
+        console.log(inputBox)
+    }
   }
-  
+
   return (
     <form>
       <input
         type="text"
         placeholder='First Name'
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder='Last Name'
         onChange={handleChange}
       />
     </form>
