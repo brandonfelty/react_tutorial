04a3663ce9e5d4d82318011610aff580e514d120
Brandon Felty <brcfelty@gmail.com>
2022-12-15 09:41:10 -0700

    handles all the form change data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    newsletter: false,
+  });
+
+  const handleChange = (e) => {
+    const { type, name, value, checked } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: type === 'checkbox' ? checked : value
+    }))
+  };
+
   return (
     <div className='login--container'>
       <div className='login--form'>
         <form id='login-form'>
-          <input type='email' placeholder='email'/>
-          <input type='password' placeholder='password'/>
-          <input type='password' placeholder='Confirm password'/>
+          <input 
+            type='email'
+            placeholder='email'
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input 
+            type='password' 
+            placeholder='password'
+            name='password'
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <input 
+            type='password' 
+            placeholder='Confirm password'
+            name='passwordConfirm'
+            value={formData.passwordConfirm}
+            onChange={handleChange}
+          />
           <div className='login-newsletter'>
-            <input type='checkbox' id='joinNewsletter'/>
+            <input 
+              type='checkbox' 
+              id='joinNewsletter'
+              name='newsletter'
+              checked={formData.newsletter}
+              onChange={handleChange}
+            />
             <label htmlFor='joinNewsletter'>I want to join the newsletter</label>
           </div>
           <button className='login--button'>Sign Up</button>
@@ -18,6 +57,6 @@ const Login = () => {
       </div>
     </div>
   )
-}
+};
 
-export default Login
+export default Login;
