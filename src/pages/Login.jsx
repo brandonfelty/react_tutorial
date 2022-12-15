2618838408c2db8b7d1e7d80fe5a681dff7cc2b6
Brandon Felty <brcfelty@gmail.com>
2022-12-15 09:12:37 -0700

    adds login button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
             <input type='checkbox' id='joinNewsletter'/>
             <label htmlFor='joinNewsletter'>I want to join the newsletter</label>
           </div>
+          <button className='login--button'>Sign Up</button>
         </form>
       </div>
     </div>
