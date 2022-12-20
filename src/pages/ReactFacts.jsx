8da423a02f00b89e70d440cc979534cc33aa0e3e
Brandon Felty <brcfelty@gmail.com>
2022-12-20 09:27:16 -0700

    add star wars comp to fact

--- src/pages/ReactFacts.jsx
+++ src/pages/ReactFacts.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar";
 import Main from "../components/Main";
 import Timer from '../components/Timer';
 import Welcome from '../components/Welcome';
+import StarWars from '../components/StarWars';
 
 const ReactFacts = () => {
   return (
@@ -10,6 +11,7 @@ const ReactFacts = () => {
       <NavBar pic='react' title='React' subtitle='Lesson 1 - React Facts'/>
       <Main />
       <Welcome />
+      <StarWars />
       <Timer />
     </>
   )
