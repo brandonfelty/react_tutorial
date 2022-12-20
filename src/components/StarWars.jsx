025a86f386967531f4bb98d50887672439ce5000
Brandon Felty <brcfelty@gmail.com>
2022-12-20 09:32:47 -0700

    sets up star wars challenge

--- src/components/StarWars.jsx
+++ src/components/StarWars.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const StarWars = () => {
+  const [starWarsData, setStarWarsData] = useState({});
+  const [character, setCharacter] = useState(1);
+
+  useEffect(() => {
+    console.log('effect ran')
+    fetch('https://swapi.dev/api/people/1')
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
+  }, []);
+
   return (
-    <div>StarWars</div>
+    <div className='starwars'>
+      <h2>Character Number: {character}</h2>
+      <button>Get Next Character</button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+    </div>
   )
 }
 
