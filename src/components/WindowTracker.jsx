8720f0f94e1740ba0b32fb4ed47e1f92ac5b2ac0
Brandon Felty <brcfelty@gmail.com>
2022-12-21 06:28:20 -0700

    button toggles window tracker

--- src/components/WindowTracker.jsx
+++ src/components/WindowTracker.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const WindowTracker = () => {
+  const [show, setShow] = useState(true);
+  
+  const handleClick = () => {
+    setShow(prev => !prev);
+  };
+
   return (
-    <div>WindowTracker</div>
+    <div className='windowtracker--container'>
+      <button onClick={handleClick}>
+        Toggle WindowTracker
+      </button>
+      { show && <h1>Window width: {window.innerWidth}</h1>}
+    </div>
   )
 }
 
-export default WindowTracker
+export default WindowTracker;
