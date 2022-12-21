f3f6254483a3004da3867374e46e956f198a4021
Brandon Felty <brcfelty@gmail.com>
2022-12-21 06:35:11 -0700

    updates inner window width with state

--- src/components/WindowTracker.jsx
+++ src/components/WindowTracker.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const WindowTracker = () => {
   const [show, setShow] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   
   const handleClick = () => {
     setShow(prev => !prev);
   };
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setWindowWidth(window.innerWidth);
+    })
+  }, []);
+
+  console.log('rendered')
 
   return (
     <div className='windowtracker--container'>
       <button onClick={handleClick}>
         Toggle WindowTracker
       </button>
-      { show && <h1>Window width: {window.innerWidth}</h1>}
+      {show && <h1>Window width: {windowWidth}</h1>}
     </div>
   )
 }
