2f7ceffd18c4108265ea950389351bdc8db14288
Brandon Felty <brcfelty@gmail.com>
2022-12-22 10:27:20 -0700

    dynamic classnames for darkmode

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './Main.css';
 import reactBackground from '../images/react.png';
 
-const Main = () => {
+const Main = ({ darkMode }) => {
+
   return (
-    <div className='main-container'>
-      <h1 className='main-title'>Fun facts about React</h1>
-      <ul className='fact-list'>
+    <div className={`main-container ${darkMode ? 'dark' : ''}`}>
+      <h1 className={`main-title ${darkMode ? 'dark' : ''}`}>Fun facts about React</h1>
+      <ul className={`fact-list ${darkMode ? 'dark' : ''}`}>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
