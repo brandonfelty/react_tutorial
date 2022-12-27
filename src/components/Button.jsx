88b88c34d6ec2ac88efdbe8b00a71fa170cd8d92
Brandon Felty <brcfelty@gmail.com>
2022-12-27 08:12:40 -0700

    change classnames for specificity

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,8 +8,8 @@ const Button = ({ type }) => {
         formAction={type === 'linkedin' ? 'https://www.linkedin.com/in/brcfelty/' : null}
         className={`${type} button-component`}
       >
-        {type === 'email' && <i className="fa-solid fa-envelope"></i>} 
-        {type === 'linkedin' && <i className="fa-brands fa-linkedin"></i>} 
+        {type === 'email' && <i className="fa-solid fa-envelope button-icon"></i>} 
+        {type === 'linkedin' && <i className="fa-brands fa-linkedin button-icon"></i>} 
         {type}
       </button>
   </form>
