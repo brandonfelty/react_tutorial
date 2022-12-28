fba1a530468a34ff53bbea5112d1196cd321610e
Brandon Felty <brcfelty@gmail.com>
2022-12-28 08:19:50 -0700

    made the me controlled

--- src/pages/MDEditor.jsx
+++ src/pages/MDEditor.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 
 const MDEditor = () => {
+  const [note, setNote] = useState('Write your markdown here..');
+  const onChange = useCallback((value) => {
+    setNote(value);
+  }, []);
+
   return (
     <div>
-      <SimpleMDE />
+      <SimpleMDE value={note} onChange={onChange} />
     </div>
   )
 };
