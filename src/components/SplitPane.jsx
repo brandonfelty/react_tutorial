8ede512b815800bb64ac0025e55eed9b43f566e8
Brandon Felty <brcfelty@gmail.com>
2023-01-04 09:18:41 -0700

    add mouse hold move function

--- src/components/SplitPane.jsx
+++ src/components/SplitPane.jsx
@@ -7,10 +7,24 @@ import React, {
 } from 'react';
 import SplitPaneContext from '../contexts/SplitPaneContext';
 
-const SplitPane = () => {
-  return (
-    <div>SplitPane</div>
-  )
-}
+const SplitPane = ({ children, ...props }) => {
+  const [clientHeight, setClientHeight] = useState(null);
+  const [clientWidth, setClientWidth] = useState(null);
+  const yDividerPos = useRef(null);
+  const xDividerPos = useRef(null);
+
+  const onMouseHoldDown = (e) => {
+    yDividerPos.current = e.clientY;
+    xDividerPos.current = e.clientX;
+  };
+
+  const onMouseHoldUp  = (e) => {
+    yDividerPos.current = null;
+    xDividerPos.current = null;
+  };
+
+  
+
+};
 
 export default SplitPane;
