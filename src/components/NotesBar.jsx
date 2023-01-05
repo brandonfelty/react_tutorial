c1ae409927a97aa282d66c543be5a6b842fcbbe7
Brandon Felty <brcfelty@gmail.com>
2023-01-05 08:39:49 -0700

    remove notes elements and no notes comp for clarity

--- src/components/NotesBar.jsx
+++ src/components/NotesBar.jsx
@@ -3,30 +3,44 @@ import React from 'react';
 import './NotesBar.css';
 
 const NotesBar = ({ notes, toggleActiveNote, activeNoteId, addNote }) => {
-  console.log(notes)
-  return (
-    <div className={`mde--sidebar ${notes.length === 0 && 'no-notes'}`}>
+  const notesElement = notes.map((note) => (
+    <div 
+      className=
+      {
+        `mde--sidebar-note 
+        ${activeNoteId === note.id ? 'active' : ''}
+        ${notes.length === 0 ? 'notes-empty' : ''}
+        `
+      }
+      key={note.id}
+      onClick={() => toggleActiveNote(note.id)}
+    >
+      {`Note ${note.id + 1}`}
+    </div>
+  ));
+
+  const NoNotes = () => {
+    return (
+      <div className='no-notes'>
       <div className='add-note'>
-        <h2 className='mde--sidebar-title'>{notes.length === 0 ? 'Add Your First Note' : 'Notes '}</h2>
-        <button className={`add-icon ${notes.length === 0 && 'large'}`} onClick={addNote}>
+        <h2 className='mde--sidebar-title'>Add Your First Note </h2>
+        <button className='add-icon large' onClick={addNote}>
           <i className="fa-solid fa-plus"></i>
         </button>
       </div>
-      { notes.length !== 0 ? notes.map((note, index) => (
-        <div 
-          className=
-          {
-            `mde--sidebar-note 
-            ${activeNoteId === note.id ? 'active' : ''}
-            ${notes.length === 0 ? 'notes-empty' : ''}
-            `
-          }
-          key={note.id}
-          onClick={() => toggleActiveNote(note.id)}
-        >
-          {`Note ${note.id + 1}`}
-        </div>
-      )) : <></>}
+    </div>
+    )
+  };
+
+  return (
+    <div className='mde--sidebar'>
+      <div className='add-note'>
+        <h2 className='mde--sidebar-title'>Notes </h2>
+        <button className='add-icon' onClick={addNote}>
+          <i className="fa-solid fa-plus"></i>
+        </button>
+      </div>
+      {notesElement}
     </div>
   )
 };
